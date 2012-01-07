6b34afb8340a511068c7de8fdbe3777506953f49
Jonny Gerig Meyer <jonny@oddbird.net>
2012-01-07 15:40:23 -0500

    updated js files

--- static/js/local.js
+++ static/js/local.js
@@ -1,5 +1,4 @@
 $(function() {
-    $('#hcard-client-name .email').defuscate();
     $('input[placeholder], textarea[placeholder]').placeholder();
     $('.details:not(html)').html5accordion('.summary');
     $('#messages').messages();
