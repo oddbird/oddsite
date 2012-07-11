914e48cc8055d589fb9b555cf5e14fef26259cae
Eric Meyer <eric@oddbird.net>
2012-07-11 16:44:31 -0600

    Strip styles

--- config.rb
+++ config.rb
@@ -7,11 +7,11 @@ project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "sass"
-css_dir = "static/css"
-images_dir = "static/images"
-fonts_dir = "static/fonts"
-javascripts_dir = "static/js"
+css_dir = "content/static/css"
+images_dir = "content/static/images"
+fonts_dir = "content/static/fonts"
+javascripts_dir = "content/static/js"
 line_comments = false
-preferred_syntax = :sass
-output_style = :compact
+preferred_syntax = :scss
+output_style = :expanded
 relative_assets = true
