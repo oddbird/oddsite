6a49f689e21ef86701e8a0bd2cc7c65f06381bc0
Jonny Gerig Meyer <jonny@oddbird.net>
2012-11-25 19:20:38 -0700

    remove jquery and modernizr

--- content/static/js/local.js
+++ content/static/js/local.js
@@ -1,5 +1,37 @@
-$(function () {
+var OBC = (function (OBC) {
+
     'use strict';
 
-    $('.contact .email').defuscate();
-});
+    OBC.defuscate = function () {
+        var find = /\b([A-Z0-9._%\-]+)\([^)]+\)((?:[A-Z0-9\-]+\.)+[A-Z]{2,6})\b/gi;
+        var replace = '$1@$2';
+        var is_link;
+        var links = document.getElementsByClassName('email');
+        var defuscateLink = function (el) {
+            var html = el.innerHTML;
+            var replacedHTML;
+            if (el.hasAttribute('href')) {
+                var replaced = el.getAttribute('href').replace(find, replace);
+                el.setAttribute('href', replaced);
+                is_link = true;
+            }
+            if (is_link) {
+                replacedHTML = html.replace(find, replace);
+            } else {
+                replacedHTML = html.replace(find, '<a href="mailto:' + replace + '">' + replace + '</a>');
+            }
+            el.innerHTML = replacedHTML;
+        };
+        var i;
+        for (i = 0; i < links.length; i++) {
+            defuscateLink(links[i]);
+        }
+    };
+
+    window.onload = function () {
+        OBC.defuscate();
+    };
+
+    return OBC;
+
+}(OBC || {}));
