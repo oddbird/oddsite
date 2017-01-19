1d8aac221818c1b7889f3237fbf2f2db7f45e3ab
Jonny Gerig Meyer <jonny@oddbird.net>
2017-01-19 16:32:40 -0500

    Add default pygments syntax-highlighting styles.

--- modules/pygments_simple.py
+++ modules/pygments_simple.py
@@ -12,8 +12,9 @@ from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 
 from pygments import highlight
-from pygments.lexers import get_lexer_by_name, TextLexer
+from pygments.lexers import get_lexer_by_name, guess_lexer, TextLexer
 from pygments.formatters import HtmlFormatter
+from pygments.util import ClassNotFound
 
 
 html_formatter = HtmlFormatter()
@@ -26,11 +27,14 @@ class CodeBlock(Directive):
     final_argument_whitespace = False
 
     def run(self):
+        code = u'\n'.join(self.content)
         try:
             lexer = get_lexer_by_name(self.arguments[0])
         except ValueError:
-            lexer = TextLexer()
-        code = u'\n'.join(self.content)
+            try:
+                lexer = guess_lexer(code)
+            except ClassNotFound:
+                lexer = TextLexer()
         formatted = highlight(code, lexer, html_formatter)
         return [nodes.raw('', formatted, format='html')]
 
