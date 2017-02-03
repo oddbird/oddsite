070edd275b02febdfecb6764fde33a35e0c889bf
Jonny Gerig Meyer <jonny@oddbird.net>
2017-02-03 10:30:39 -0500

    add og:url for archive pages

--- modules/urls.py
+++ modules/urls.py
@@ -6,12 +6,11 @@ Get full canonical URLs of pages for OpenGraph tags.
 from urlparse import urljoin
 
 
-def get_full_canonical_url(context):
-    root = context.builder.config['canonical_url']
-    slug = context.slug
+def get_full_canonical_url(builder, slug='/'):
+    root = builder.config['canonical_url']
     if slug.endswith('index'):
         slug = slug.rstrip('index')
-    url = urljoin(root, context.builder.link_to('page', slug=slug))
+    url = urljoin(root, builder.link_to('page', slug=slug))
     if not url.endswith('/'):
         url += '/'
     return url
