a738aab06f84f23842c1d3db24289345798d954e
Jonny Gerig Meyer <jonny@oddbird.net>
2017-02-27 17:48:38 -0500

    Update GA spam-referrals on "gulp prod"; filter out local traffic.

---
+++ static/js/spamReferralsBlocker.js
@@ -0,0 +1,15 @@
+import spammers from 'raw-loader!./spammers.txt';
+
+window.isSpamReferral = function () {
+  const list = spammers.split('\n');
+  const currentReferral = document.referrer;
+  if (currentReferral) {
+    list.unshift('oddsite.hexxie.com', 'localhost');
+    for (const spammer of list) {
+      if (spammer && currentReferral.toLowerCase().indexOf(spammer) !== -1) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
