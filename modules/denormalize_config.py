f90267f821bff373e3f9399a7f1d745913742c17
Kit La Touche <kit@transneptune.net>
2017-03-31 15:46:52 -0600

    Allow oss_by_author to pull from per-page config

--- modules/denormalize_config.py
+++ modules/denormalize_config.py
@@ -8,11 +8,21 @@ from collections import defaultdict
 def setup(builder):
     config = builder.config
 
+    pages = [
+        page
+        for page
+        in builder.iter_contexts()
+        if 'contributors' in page.config
+    ]
     # Add OSS projects indexed by author.
     author_projects = defaultdict(list)
     for project in config['oss']:
         if 'who' in project:
             for author in project['who']:
                 author_projects[author['author']].append(project)
+
+    for page in pages:
+        for author in page.config['contributors']:
+            author_projects[author['author']].append(page.config['project'][0])
 
     builder.config.stack[-1]['oss_by_author'] = author_projects
