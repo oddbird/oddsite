624ae669049e963a51815be1e91fff8af4e483e6
Miriam Suzanne <miriam@oddbird.net>
2017-06-27 15:20:05 -0600

    Copy susydocs into oddsite and clean up links

---
+++ content/susy/docs/assets/js/init.js
@@ -0,0 +1,6 @@
+window.Herman = (function(i, n) {
+  'use strict';
+  return n(function() {
+    window.hljs.initHighlightingOnLoad(), i.initializeToggles(), i.initializeTabs(), i.initializeIframes();
+  }), i;
+})(window.Herman || {}, window.jQuery);
