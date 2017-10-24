ff7af7a987b534a7397eced0b3d25630fe0c31ae
Miriam Suzanne <miriam@oddbird.net>
2017-10-24 11:26:18 -0600

    Add rst and typogriphy filters to jinja2

---
+++ modules/rst.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Add an |rst filter to the jinja templates…
+"""
+
+from docutils.core import publish_parts
+from jinja2 import Markup
+
+
+def rst_filter(s):
+    return Markup(publish_parts(source=s, writer_name='html')['body'])
+
+
+def setup(builder):
+    env = builder.jinja_env
+    env.filters['rst'] = rst_filter
